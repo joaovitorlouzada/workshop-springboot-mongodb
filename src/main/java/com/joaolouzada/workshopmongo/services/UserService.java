040df6b74a2734076eca5680a0abd71edce6125b
Joao Vitor <joaovitorlouzada@hotmail.com>
2025-09-01 10:27:13 -0300

    Atualizacao do usuario com PUT

--- src/main/java/com/joaolouzada/workshopmongo/services/UserService.java
+++ src/main/java/com/joaolouzada/workshopmongo/services/UserService.java
@@ -1,6 +1,7 @@
 package com.joaolouzada.workshopmongo.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -34,6 +35,18 @@ public class UserService {
 		repo.deleteById(id);
 	}
 	
+	public User update(User obj) {
+	    User newObj = repo.findById(obj.getId())
+	                      .orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	    updateData(newObj, obj);
+	    return repo.save(newObj);
+	}
+
+	private void updateData(User newObj, User obj) {
+	    newObj.setName(obj.getName());
+	    newObj.setEmail(obj.getEmail());
+	}
+
 	public User fromDTO(UserDTO userDto) {
 		return new User(userDto.getId(), userDto.getName(), userDto.getEmail());
 	}
