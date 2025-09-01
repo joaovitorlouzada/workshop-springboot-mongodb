785b8815e1ff9739e72c75b407595a50796cecc5
Joao Vitor <joaovitorlouzada@hotmail.com>
2025-09-01 10:02:45 -0300

    Obtendo um usuário por id

--- src/main/java/com/joaolouzada/workshopmongo/services/UserService.java
+++ src/main/java/com/joaolouzada/workshopmongo/services/UserService.java
@@ -1,12 +1,14 @@
 package com.joaolouzada.workshopmongo.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.joaolouzada.workshopmongo.domain.User;
 import com.joaolouzada.workshopmongo.repositories.UserRepository;
+import com.joaolouzada.workshopmongo.services.exceptions.ObjectNotFoundException;
 
 @Service
 public class UserService {
@@ -17,6 +19,9 @@ public class UserService {
 	
 	public List<User> findAll(){
 		return repo.findAll();
+	}
+	public User findById(String id) {
+	    return repo.findById(id).orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
 	}
 
 }
