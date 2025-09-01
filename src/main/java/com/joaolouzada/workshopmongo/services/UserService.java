00dca03f6bd7e5b9ed871730569d4c5f95c40a29
Joao Vitor <joaovitorlouzada@hotmail.com>
2025-09-01 10:12:54 -0300

    Insercao de usuario com POST

--- src/main/java/com/joaolouzada/workshopmongo/services/UserService.java
+++ src/main/java/com/joaolouzada/workshopmongo/services/UserService.java
@@ -1,12 +1,12 @@
 package com.joaolouzada.workshopmongo.services;
 
 import java.util.List;
-import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.joaolouzada.workshopmongo.domain.User;
+import com.joaolouzada.workshopmongo.dto.UserDTO;
 import com.joaolouzada.workshopmongo.repositories.UserRepository;
 import com.joaolouzada.workshopmongo.services.exceptions.ObjectNotFoundException;
 
@@ -22,6 +22,12 @@ public class UserService {
 	}
 	public User findById(String id) {
 	    return repo.findById(id).orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+	public User insert(User obj) {
+		return repo.insert(obj);
+	}
+	public User fromDTO(UserDTO userDto) {
+		return new User(userDto.getId(), userDto.getName(), userDto.getEmail());
 	}
 
 }
